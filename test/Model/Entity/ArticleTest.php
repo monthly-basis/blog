9697fc09d76220593fa7a7395460369361a68610
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-08-22 01:07:17 +0000

    Write preliminary test for article entity

---
+++ test/Model/Entity/ArticleTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace LeoGalleguillos\BlogTest\Model\Entity;
+
+use DateTime;
+use LeoGalleguillos\Blog\Model\Entity as BlogEntity;
+use LeoGalleguillos\User\Model\Entity as UserEntity;
+use PHPUnit\Framework\TestCase;
+
+class ArticleTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->articleEntity = new BlogEntity\Article();
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            BlogEntity\Article::class,
+            $this->articleEntity
+        );
+    }
+}
