750e06c1e475d96ba68a7982b368b3ee3ffddd3b
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-05-19 11:35:39 +0000

    Add sql to add foreign key constraint

--- sql/v0.0.2/article.sql
+++ sql/v0.0.2/article.sql
@@ -1,1 +1,7 @@
+ALTER TABLE `article` ADD
+  CONSTRAINT FOREIGN KEY (`blog_id`)
+    REFERENCES `blog` (`blog_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+;
 ALTER TABLE `article` ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
