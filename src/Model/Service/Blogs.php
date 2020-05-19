48f106fcc205523c56fe6217a0fb0489f7c3e727
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-05-19 11:03:05 +0000

    Exclude deleted blogs when listing blogs

--- src/Model/Service/Blogs.php
+++ src/Model/Service/Blogs.php
@@ -5,32 +5,25 @@ use Generator;
 use Laminas\Db as LaminasDb;
 use LeoGalleguillos\Blog\Model\Entity as BlogEntity;
 use LeoGalleguillos\Blog\Model\Factory as BlogFactory;
-use LeoGalleguillos\Blog\Model\Service as BlogService;
 use LeoGalleguillos\Blog\Model\Table as BlogTable;
 
 class Blogs
 {
     public function __construct(
         BlogFactory\Blog $blogFactory,
-        BlogTable\Blog $blogTable,
-        LaminasDb\TableGateway\TableGateway $blogTableGateway
+        BlogTable\Blog $blogTable
     ) {
-        $this->blogFactory      = $blogFactory;
-        $this->blogTable        = $blogTable;
-        $this->blogTableGateway = $blogTableGateway;
+        $this->blogFactory = $blogFactory;
+        $this->blogTable   = $blogTable;
     }
 
     /**
-     * Get blogs.
-     *
      * @yield BlogEntity\Blog
-     * @return Generator
      */
-    public function getBlogs() : Generator
+    public function getBlogs(): Generator
     {
-        $generator = $this->blogTable->selectOrderByCreatedDesc();
-        foreach ($generator as $array) {
-            yield $this->blogFactory->buildFromArray($array);
+        foreach ($this->blogTable->select() as $arrayObject) {
+            yield $this->blogFactory->buildFromArray((array) $arrayObject);
         }
     }
 }
---
+++ test/Model/Service/BlogsTest.php
@@ -0,0 +1,62 @@
+<?php
+namespace LeoGalleguillos\BlogTest\Model\Service;
+
+use Laminas\Db as LaminasDb;
+use LeoGalleguillos\Blog\Model\Entity as BlogEntity;
+use LeoGalleguillos\Blog\Model\Factory as BlogFactory;
+use LeoGalleguillos\Blog\Model\Service as BlogService;
+use LeoGalleguillos\Blog\Model\Table as BlogTable;
+use LeoGalleguillos\Test\Hydrator as TestHydrator;
+use PHPUnit\Framework\TestCase;
+
+class BlogsTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->blogFactoryMock = $this->createMock(
+            BlogFactory\Blog::class
+        );
+        $this->blogTableMock = $this->createMock(
+            BlogTable\Blog::class
+        );
+        $this->blogsService = new BlogService\Blogs(
+            $this->blogFactoryMock,
+            $this->blogTableMock
+        );
+    }
+
+    public function test_getBlogs()
+    {
+        $hydrator = new TestHydrator\CountableIterator();
+        $resultSetMock = $this->createMock(
+            LaminasDb\ResultSet\ResultSet::class
+        );
+        $hydrator->hydrate(
+            $resultSetMock,
+            [
+                ['slug' => 'slug1'],
+                ['slug' => 'slug2'],
+            ]
+        );
+        $blog1 = new BlogEntity\Blog();
+        $blog2 = new BlogEntity\Blog();
+        $this->blogTableMock
+            ->expects($this->once())
+            ->method('select')
+            ->willReturn($resultSetMock);
+        $this->blogFactoryMock
+            ->expects($this->exactly(2))
+            ->method('buildFromArray')
+            ->withConsecutive(
+                [['slug' => 'slug1']],
+                [['slug' => 'slug2']]
+            )->will(
+                $this->onConsecutiveCalls(
+                    $blog1,
+                    $blog2
+                )
+            );
+        $generator = $this->blogsService->getBlogs();
+        $blogs = iterator_to_array($generator);
+    }
+}
