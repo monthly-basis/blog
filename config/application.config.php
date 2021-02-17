7bb91f09e67356e8f32275858efefad1ac4c562b
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-02-17 03:45:44 +0000

    Replace Zend with Laminas

--- config/application.config.php
+++ config/application.config.php
@@ -2,12 +2,12 @@
 
 return [
     'modules' => [
+        'Laminas\Db',
+        'Laminas\Router',
         'LeoGalleguillos\Blog',
         'MonthlyBasis\Flash',
         'MonthlyBasis\String',
         'MonthlyBasis\User',
-        'Zend\Db',
-        'Zend\Router',
     ],
     // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => array(
